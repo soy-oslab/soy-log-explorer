05e37e21a389b67c1624effb583483b3ac2293e2
SuhoSon <ngeol564@gmail.com>
2021-06-30 08:43:54 +0000

    feat: Dockerize github action
    Adding the github action Dockerize

---
+++ scripts/soy_log_explorer_gen_src_archive.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+PROJECT=soy_log_explorer
+DEST="build/${PROJECT}.src.tar.gz"
+
+# Check that git-archive-all is installed
+if ! git-archive-all --help &> /dev/null; then
+  echo "# This script uses 'git-archive-all', please install:"
+  echo "# pip install git-archive-all"
+  exit 1
+fi
+
+mkdir build
+git-archive-all --prefix="soy_log_explorer" --force-submodules $DEST
+
+echo "# $0: DONE!"
+ls -lh $DEST
